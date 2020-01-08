4b915d57332bdc50726ab798e3c8413151fe7e91
Ulises <alejendro-ulises.gonzalez@epitech.eu>
2020-01-08 20:03:48 +0100

    Final

--- src/main.py
+++ src/main.py
@@ -28,43 +28,52 @@ def main():
         pc = sys.argv[7]
         pb = sys.argv[8]
         ps = sys.argv[9]
+        
         print("Resources: " + n[0] + " F1, " + n[1] + " F2, " + n[2]+ " F3, " + n[3] + " F4\n")
 
         names = ["Oat", "Wheat", "Corn", "Barley", "Soy"]
 
-        count = 0
+        #count = 0
         # 1
         matrix = func.make_matrix(n[0], n[1], n[2], n[3], po, pw, pc, pb, ps)
         # while 문
         while func.loop_check_zero(matrix):
-            print("Current state: ")
-            func.matrix_print(matrix)
             # 2
             pivot_col_num = func.find_max_neg_col(matrix)
-            print("pivot_col_num: ", pivot_col_num)
+            #print("pivot_col_num: ", pivot_col_num)
             col = func.sel_col(matrix, pivot_col_num)
             # 3
             pivot_row_num = func.sel_pivot_row_num(col, matrix)
-            print("pivot_row_num: ", pivot_row_num)
+            #print("pivot_row_num: ", pivot_row_num)
             # 4
             pivot_ele = matrix[pivot_row_num][pivot_col_num]
             func.pivot_one(pivot_row_num, matrix, pivot_col_num, pivot_ele)
-            print("matrix_pivot_one: ")
-            func.matrix_print(matrix)
+            #print("matrix_pivot_one: ")
+            #func.matrix_print(matrix)
             # 5
-            other_zero_matrix = func.make_other_zero(matrix, pivot_row_num, pivot_col_num)
-            matrix = other_zero_matrix
-            print("five stage end")
-            func.matrix_print(matrix)
-            count += 1
-            print("count : " , count)
-        print("loop finish")
-        func.matrix_print(matrix)
-
-        # for i in range(len(names)):
-        #     print(names[i] + ": " + " units at $" + sys.argv[5 + i] + "/unit")
-        #
-        # print("\nTotal production value:" + " $")
+            func.make_other_zero(matrix, pivot_row_num, pivot_col_num)
+            #print("five stage end:")
+            #func.matrix_print(matrix)
+            #count += 1
+            #print("count: " , count)
+        #print("loop finish")
+        #func.matrix_print(matrix)
+        quantities = [0,0,0,0,0]
+        for i in range(5):
+            col = func.sel_col(matrix,i)
+            if col.count(1) == 1 and col.count(0) == len(matrix) - 1:
+                quantities[i] = round(matrix[col.index(1)][len(matrix[0]) - 1],3)
+                if quantities[i] == 0:
+                    quantities[i] = int(quantities[i])
+        for i in range(len(names)):
+            if quantities[i] == 0:
+                print(names[i] , ": 0 units at $" , sys.argv[5 + i] , "/unit", sep='')
+            else:
+                print(names[i] , ": " , "{0:.2f}".format(quantities[i]), " units at $" , sys.argv[5 + i] , "/unit", sep='')
+        total = 0
+        for i in range(5):
+            total +=  quantities[i] * float(sys.argv[5 + i])
+        print("\nTotal production value:" , "{0:.2f}".format(total), " $")
 
 
 if __name__ == '__main__':
